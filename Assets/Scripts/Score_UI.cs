d6161ca386ba755858acd7e9e2976a31f1642c1d
Venera <31911811+venera-program@users.noreply.github.com>
2025-08-19 14:51:49 -0700

    script for Score UI

---
+++ Assets/Scripts/Score_UI.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+[RequireComponent(typeof(TMPro.TMP_Text))]
+public class Score_UI : MonoBehaviour
+{
+    private TMPro.TMP_Text _scoreText;
+
+    private void Awake()
+    {
+        _scoreText = GetComponent<TMPro.TMP_Text>();
+    }
+
+    private void OnEnable()
+    {
+        UpdateScoreText();
+        PlayerData.OnScoreChanged += UpdateScoreText;
+    }
+
+    private void OnDisable()
+    {
+        PlayerData.OnScoreChanged -= UpdateScoreText;
+    }
+
+    private void UpdateScoreText()
+    {
+        _scoreText.text = PlayerData.Score.ToString();
+    }
+}
