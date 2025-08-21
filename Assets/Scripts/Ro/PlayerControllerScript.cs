c0f52ec3202d4502dac9f8ac4368b83e03378b33
Rohaid <rohaidb@protonmail.com>
2025-08-21 18:35:55 -0400

    Player Controls
    - Holding the button allows projectiles to be spawned

--- Assets/Scripts/Ro/PlayerControllerScript.cs
+++ Assets/Scripts/Ro/PlayerControllerScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem.Interactions;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerControllerScript : MonoBehaviour
@@ -18,6 +19,13 @@ public class PlayerControllerScript : MonoBehaviour
    public float accel;
    public float deccel;
 
+   private float projectileTimer = 0f;
+
+   [SerializeField]
+   private float projectileSpawnInterval;
+   private bool startProjectSpawnTimer = false;
+   private bool startGeneratingProject = false;
+
    
    void Awake(){
         if(instance != null && instance != this){
@@ -29,12 +37,27 @@ public class PlayerControllerScript : MonoBehaviour
         controller = new PlayerController();
         rb = GetComponent<Rigidbody2D>();
         controller.Enable();
-        controller.Main.Shoot.performed += Shoot;
+        controller.Main.Shoot.started += Shoot;
+        controller.Main.Shoot.canceled += StopShoot;
+        projectileTimer = projectileSpawnInterval;
    }
 
    void OnDisable(){
-     controller.Main.Shoot.performed -= Shoot;
+     controller.Main.Shoot.started -= Shoot;
+     controller.Main.Shoot.canceled -= StopShoot;
      controller.Disable();
+   }
+
+   void Update(){
+          if (startProjectSpawnTimer){
+               projectileTimer += Time.deltaTime;
+               if (startGeneratingProject){
+                    if (projectileTimer > projectileSpawnInterval){
+                         projectileTimer = 0f;
+                         GeneratePlayerProjectiles();
+                    }
+               }
+          }   
    }
 
    void FixedUpdate(){
@@ -53,7 +76,20 @@ public class PlayerControllerScript : MonoBehaviour
    }
 
    private void Shoot(UnityEngine.InputSystem.InputAction.CallbackContext cont){
-          for(int i = 0 ; i < shootingPattern.Length ; i++){
+     startProjectSpawnTimer = true;
+     if (cont.interaction is HoldInteraction) {
+          startGeneratingProject = true;
+     }
+   }
+
+   private void StopShoot(UnityEngine.InputSystem.InputAction.CallbackContext cont){
+     if(cont.interaction is HoldInteraction){
+          startGeneratingProject = false;
+     }
+   }
+
+   private void GeneratePlayerProjectiles(){
+      for(int i = 0 ; i < shootingPattern.Length ; i++){
                float rad = shootingPattern[i].startingAngle * Mathf.Deg2Rad;
                float xPos = Mathf.Cos(rad) * shootingPattern[i].radius;
                float yPos = Mathf.Sin(rad) * shootingPattern[i].radius;
