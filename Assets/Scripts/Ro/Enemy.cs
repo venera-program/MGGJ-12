46d0253049a064fc5d62c3dc83e700df2c88ce6e
ninelevitzki <ninelevitzki@gmail.com>
2025-09-07 15:21:49 -0600

    Update Enemy.cs
    enemy stops moving when it dies

--- Assets/Scripts/Ro/Enemy.cs
+++ Assets/Scripts/Ro/Enemy.cs
@@ -8,12 +8,14 @@ public class Enemy : MonoBehaviour
     private Animator animator;
     [SerializeField] private float deathDelay;
     private Collider2D collider;
+    private EnemyMotor script;
 
     void Awake()
     {
         GetComponent<Health>().healthChange.AddListener(OnDeath);
         animator = this.GetComponentInChildren<Animator>();
         collider = GetComponent<Collider2D>();
+        script = GetComponent<EnemyMotor>();
     }
 
     public void OnDeath(float currHealth, float maxHealth)
@@ -21,6 +23,7 @@ public class Enemy : MonoBehaviour
         if (currHealth <= 0)
         {
             collider.enabled = false;
+            script.speed = 0;
             AudioManager.Instance.PlayEnemyDies_SFX();
             animator.Play("Dead");
             PlayerData.UpdateScore(score);
