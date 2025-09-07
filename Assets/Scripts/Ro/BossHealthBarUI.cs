5582ccdc1c6c9058ef05e47b8f66c9e540455ddd
Venera <31911811+venera-program@users.noreply.github.com>
2025-09-07 11:09:15 -0700

    Compilation error fix

--- Assets/Scripts/Ro/BossHealthBarUI.cs
+++ Assets/Scripts/Ro/BossHealthBarUI.cs
@@ -3,7 +3,8 @@ using System;
 using System.Collections;
 using UnityEngine.UI;
 
-public class BossHealthBarUI : MonoBehaviour {
+public class BossHealthBarUI : MonoBehaviour
+{
 
     [SerializeField] private Slider bossHealthSlider;
     [SerializeField] private GameObject sliderObject;
@@ -12,37 +13,47 @@ public class BossHealthBarUI : MonoBehaviour {
 
     public static BossHealthBarUI instance;
 
-    void Awake(){
+    void Awake()
+    {
         sliderObject.SetActive(false);
-        if(instance != null && instance != this){
+        if (instance != null && instance != this)
+        {
             Destroy(gameObject);
-        } else {
+        }
+        else
+        {
             instance = this;
         }
     }
 
-    void Start(){
-        Enemy_Spawner.Instance.bossSpawned.AddListener(TurnOnBossHealthBar);
+    void Start()
+    {
+        Enemy_Spawner.Instance.BossSpawned.AddListener(TurnOnBossHealthBar);
     }
 
-    private void TurnOnBossHealthBar(){
+    private void TurnOnBossHealthBar()
+    {
         sliderObject.SetActive(true);
         bossHealthSlider.value = 1f;
     }
 
-    public void UpdateBossHealthBar(float currentHealth , float maxHealth){
-        if(!bossHealthSlider) return;
-        if(currentHealth == 0){
+    public void UpdateBossHealthBar(float currentHealth, float maxHealth)
+    {
+        if (!bossHealthSlider) return;
+        if (currentHealth == 0)
+        {
             TurnOffHealthBar();
         }
-        bossHealthSlider.value = currentHealth/maxHealth;
+        bossHealthSlider.value = currentHealth / maxHealth;
     }
 
-    private void TurnOffHealthBar(){
+    private void TurnOffHealthBar()
+    {
         StartCoroutine(TurnOff(turnOffDelay));
     }
 
-    private IEnumerator TurnOff(float delay){
+    private IEnumerator TurnOff(float delay)
+    {
         yield return new WaitForSeconds(delay);
         bossHealthSlider.value = 0;
         sliderObject.SetActive(false);
